af367c236b081a43615743abe39f8fc31ba81841
Miguel Fernandez <miguelfera@gmail.com>
2022-11-09 14:50:03 -0600

    Initialize letter to solve

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import allWords from "./db.json";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        //random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is {solution}</div>}
     </div>
   );
 }
